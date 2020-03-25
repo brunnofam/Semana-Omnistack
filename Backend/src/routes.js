a7f525e5291c45021cb5aa40eb3cc6eb396176c7
brunnofam <fazziobruno@hotmail.com>
2020-03-25 03:57:53 -0300

    Back-End

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,11 +1,23 @@
 const express = require("express");
 
 const OngController = require("./controllers/OngController");
+const incidentController = require("./controllers/incidentController");
+const profileControler = require("./controllers/profileControler");
+const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
+routes.post("/session", SessionController.create);
+
+
 routes.get("/ongs", OngController.index); 
 routes.post("/ongs", OngController.create);
+
+routes.get("/profile", profileControler.index); 
+
+routes.get("/incidents", incidentController.index);
+routes.post("/incidents", incidentController.create);
+routes.delete("/incidents/:id", incidentController.delete);
 
 module.exports = routes; 
 
